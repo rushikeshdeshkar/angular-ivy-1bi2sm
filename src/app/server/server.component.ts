f1715e1cd48a893f7b92ada4465b05a52aab9dca
rushikeshdeshkar <rdeshkar17@gmail.com>
2022-07-12 12:42:52 +0530

    Initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pp-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {
+  id: 10;
+  status: 'active';
+}
